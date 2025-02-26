a1fdf100005c68e7a7908417eeea16f8341bc145
Joshua Gilman <joshuagilman@gmail.com>
2025-02-26 11:17:25 -0800

    chore: fixes tor install

--- build.sh
+++ build.sh
@@ -6,5 +6,20 @@ set -ouex pipefail
 dnf install -y tor zlib-devel wireguard-tools
 
 ### Install Unofficial RPM packages
+
+## Tor
+tee /etc/yum.repos.d/tor.repo <<EOF
+[tor]
+name=Tor Repository
+baseurl=https://rpm.torproject.org/fedora/\$releasever/\$basearch/
+enabled=1
+gpgcheck=1
+gpgkey=https://rpm.torproject.org/rpm/public.gpg
+EOF
+
+dnf update
+dnf install tor
+
+## Bisq
 curl -Lo bisq.rpm https://github.com/bisq-network/bisq2/releases/download/v2.1.6/Bisq-2.1.6.rpm
 rpm -i bisq.rpm

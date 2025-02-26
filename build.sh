00f6ba4d168c9d13eab12052768c990225140b57
Joshua Gilman <joshuagilman@gmail.com>
2025-02-26 11:02:40 -0800

    chore: adds bisq

--- build.sh
+++ build.sh
@@ -2,5 +2,9 @@
 
 set -ouex pipefail
 
-### Install packages
+### Install RPM packages
 dnf install -y zlib-devel wireguard-tools
+
+### Install Unofficial RPM packages
+curl -Lo bisq.rpm https://github.com/bisq-network/bisq2/releases/download/v2.1.6/Bisq-2.1.6.rpm
+rpm -i bisq.rpm

efb8ca89ae2652c9fa9ba75fca94ee1b760bd359
Joshua Gilman <joshuagilman@gmail.com>
2025-02-26 11:12:51 -0800

    chore: adds tor

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -ouex pipefail
 
 ### Install RPM packages
-dnf install -y zlib-devel wireguard-tools
+dnf install -y tor zlib-devel wireguard-tools
 
 ### Install Unofficial RPM packages
 curl -Lo bisq.rpm https://github.com/bisq-network/bisq2/releases/download/v2.1.6/Bisq-2.1.6.rpm
